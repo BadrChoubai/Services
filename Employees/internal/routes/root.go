44fe1c6a043ccdc614679a7f679d150fc2b29fcb
Badr <choubai.badr@protonmail.com>
2025-03-11 15:26:50 -0600

    add root route

--- Employees/internal/routes/root.go
+++ Employees/internal/routes/root.go
@@ -1,13 +1,13 @@
 package routes
 
 import (
-  "net/http"
+	"net/http"
 )
 
 func RootHandler() http.Handler {
-  return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    w.WriteHeader(http.StatusOK)
-    _, _ = w.Write([]byte(`{"message": "Hello World"}`))
-  })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte(`{"message": "Hello World"}`))
+	})
 }
