d54949c9ffb98b5321121534cac1fbe0d87f05f6
Badr <choubai.badr@protonmail.com>
2025-03-12 12:14:37 -0600

    employees data

--- Employees/internal/data/employees.go
+++ Employees/internal/data/employees.go
@@ -3,31 +3,31 @@ package data
 import "github.com/google/uuid"
 
 type Employee struct {
-  Id        uuid.UUID `json:"id"`
-  FirstName string    `json:"firstName"`
-  LastName  string    `json:"lastName"`
-  Facility  Facility  `json:"facility"`
+	Id        uuid.UUID `json:"id"`
+	FirstName string    `json:"firstName"`
+	LastName  string    `json:"lastName"`
+	Facility  Facility  `json:"facility"`
 }
 
 type Facility struct {
-  Id       uuid.UUID    `json:"id"`
-  Alias    string       `json:"alias"`
-  Type     FacilityType `json:"type"`
-  Location Location     `json:"location"`
+	Id       uuid.UUID    `json:"id"`
+	Alias    string       `json:"alias"`
+	Type     FacilityType `json:"type"`
+	Location Location     `json:"location"`
 }
 
 type FacilityType int64
 
 const (
-  SortCenter FacilityType = iota + 1
-  DistributionCenter
+	SortCenter FacilityType = iota + 1
+	DistributionCenter
 )
 
 type Location struct {
-  Id          string `json:"id"`
-  Address     string `json:"address"`
-  City        string `json:"city"`
-  StateCode   string `json:"stateCode"`
-  PostalCode  string `json:"postalCode"`
-  CountryCode string `json:"countryCode"`
+	Id          string `json:"id"`
+	Address     string `json:"address"`
+	City        string `json:"city"`
+	StateCode   string `json:"stateCode"`
+	PostalCode  string `json:"postalCode"`
+	CountryCode string `json:"countryCode"`
 }
--- Employees/internal/repository/employees.go
+++ Employees/internal/repository/employees.go
@@ -1,30 +1,30 @@
 package repository
 
 import (
-  "context"
-  "database/sql"
-  "github.com/badrchoubai/Services/Employees/internal/data"
-  "github.com/google/uuid"
-  "time"
+	"context"
+	"database/sql"
+	"github.com/badrchoubai/Services/Employees/internal/data"
+	"github.com/google/uuid"
+	"time"
 )
 
 var _ Repository = (*EmployeesRepository)(nil)
 
 type Repository interface {
-  GetAll() ([]*data.Employee, error)
+	GetAll() ([]*data.Employee, error)
 }
 type EmployeesRepository struct {
-  db *sql.DB
+	db *sql.DB
 }
 
 func NewEmployeesRepository(db *sql.DB) *EmployeesRepository {
-  return &EmployeesRepository{
-    db: db,
-  }
+	return &EmployeesRepository{
+		db: db,
+	}
 }
 
 func (r *EmployeesRepository) GetAll() ([]*data.Employee, error) {
-  query := `
+	query := `
 SELECT 
     e.id, e.first_name, e.last_name, 
     f.id AS id, f.alias, f.type, 
@@ -35,57 +35,57 @@ LEFT JOIN main.locations l ON f.location_id = l.id;
 
 `
 
-  ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-  defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
 
-  rows, err := r.db.QueryContext(ctx, query)
-  if err != nil {
-    return nil, err
-  }
+	rows, err := r.db.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
 
-  defer rows.Close()
+	defer rows.Close()
 
-  var employees []*data.Employee
-  var facilityId sql.NullString
-  var locationId sql.NullString
+	var employees []*data.Employee
+	var facilityId sql.NullString
+	var locationId sql.NullString
 
-  if rows.Next() {
-    var employee data.Employee
-    var facility data.Facility
-    var location data.Location
+	if rows.Next() {
+		var employee data.Employee
+		var facility data.Facility
+		var location data.Location
 
-    err := rows.Scan(
-      &employee.Id,
-      &employee.FirstName,
-      &employee.LastName,
-      &facilityId,
-      &facility.Alias,
-      &facility.Type,
-      &locationId,
-      &location.Address,
-      &location.City,
-      &location.CountryCode,
-      &location.PostalCode,
-      &location.StateCode,
-    )
-    if err != nil {
-      return nil, err
-    }
+		err := rows.Scan(
+			&employee.Id,
+			&employee.FirstName,
+			&employee.LastName,
+			&facilityId,
+			&facility.Alias,
+			&facility.Type,
+			&locationId,
+			&location.Address,
+			&location.City,
+			&location.CountryCode,
+			&location.PostalCode,
+			&location.StateCode,
+		)
+		if err != nil {
+			return nil, err
+		}
 
-    // Only assign facility if it exists
-    if facilityId.Valid {
-      facility.Id = uuid.MustParse(facilityId.String)
+		// Only assign facility if it exists
+		if facilityId.Valid {
+			facility.Id = uuid.MustParse(facilityId.String)
 
-      // Only assign location if it exists
-      if locationId.Valid {
-        location.Id = locationId.String
-        facility.Location = location
-      }
-      employee.Facility = facility
-    }
+			// Only assign location if it exists
+			if locationId.Valid {
+				location.Id = locationId.String
+				facility.Location = location
+			}
+			employee.Facility = facility
+		}
 
-    employees = append(employees, &employee)
-  }
+		employees = append(employees, &employee)
+	}
 
-  return employees, nil
+	return employees, nil
 }
