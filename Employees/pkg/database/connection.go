5af16b79616dbb432ad824574cb325e58459592a
Badr <choubai.badr@protonmail.com>
2025-03-11 18:47:59 -0600

    install and setup sqlite

--- Employees/pkg/database/connection.go
+++ Employees/pkg/database/connection.go
@@ -1,36 +1,42 @@
-package sqlite
+package database
 
 import (
-  "context"
-  "database/sql"
-  "time"
+	"context"
+	"database/sql"
+	"github.com/badrchoubai/Services/Employees/pkg/observability/logging"
+	"os"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 const (
-  maxOpenConns = 5
-  maxLifetime  = 120
-  maxIdleConns = 2
-  maxIdleTime  = 20
+	maxOpenConns = 5
+	maxLifetime  = 120
+	maxIdleConns = 2
+	maxIdleTime  = 20
 )
 
 func Open() (*sql.DB, error) {
-  db, err := sql.Open("postgres", cfg.db.dsn)
-  if err != nil {
-    return nil, err
-  }
+	logger := logging.NewLogger(os.Stdout)
+	db, err := sql.Open("sqlite3", ".db/Employees.db")
+	if err != nil {
+		logger.Error("opening database", err)
+		return nil, err
+	}
 
-  db.SetConnMaxLifetime(maxLifetime)
-  db.SetMaxOpenConns(maxOpenConns)
-  db.SetMaxIdleConns(maxIdleConns)
-  db.SetConnMaxIdleTime(maxLifetime)
+	db.SetConnMaxLifetime(maxLifetime)
+	db.SetMaxOpenConns(maxOpenConns)
+	db.SetMaxIdleConns(maxIdleConns)
+	db.SetConnMaxIdleTime(maxIdleTime)
 
-  ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-  defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-  err = db.PingContext(ctx)
-  if err != nil {
-    return nil, err
-  }
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
 
-  return db, nil
+	return db, nil
 }
